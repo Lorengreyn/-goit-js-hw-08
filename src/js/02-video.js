59fe7ee8feb36aa496e980494a21db6a3e1b3e5f
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-01-22 15:10:52 +0200

    pre

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
+import Player from "@vimeo/player";
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
